4c6ef8505a9200fed8a8dca0fee48e1684758d9d
Isaac Plutzer <plutzer@isaacs-air.dhcp.wustl.edu>
2019-02-01 12:55:13 -0600

    worked on protyping algorithm

--- aggregate/algorithm_tester.py
+++ aggregate/algorithm_tester.py
@@ -1,6 +1,14 @@
+import os
 from skimage.io import imread
-import aggregates_algorithms
+from zplab.worm_analysis.aggregate import aggregates_algorithms
 import numpy as np
 from matplotlib import pyplot as plt
 
 # Get small set of test images and load them in.
+def run():
+	prototype_dir = os.getcwd() + '/zplab/worm_analysis' + '/testdata/prototype_data'
+	for img in os.listdir(prototype_dir):
+		image = imread(prototype_dir + '/' + img,as_grey = True)
+		out1, out2, out3 = aggregates_algorithms.adap_threshold(img,0.15) ## Change this call as needed.
+		print(args)
+	return
