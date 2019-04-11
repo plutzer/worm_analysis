1ceef6ba6219511d2fc8281cbd38d02481e5cda0
Pincus Lab <pincuslab@zpl-imac-1.wucon.wustl.edu>
2019-04-11 18:00:24 -0500

    updated batch script to the version on the cluster

--- cluster_code/batch_script.sh
+++ cluster_code/batch_script.sh
@@ -1,14 +1,14 @@
-#PBS -l nodes=1:ppn=1,walltime=23:00:00
-#PBS -N aggregates_algorithms_testing_Worm1_001
-#PBS -M plutzer@wuslt.edu
-#PBS -d /home/plutzer/
-#PBS -e /home/plutzer/job_log_files/
-#PBS -o /home/plutzer/job_log_files/
-cd /home/plutzer/code
+	#PBS -l nodes=1:ppn=1,walltime=47:00:00
+	#PBS -N aggregates_algorithms_testing_Worm1_001
+	#PBS -M plutzer@wuslt.edu
+	#PBS -d /scratch/plutzer/
+	#PBS -e /home/plutzer/job_log_files/
+	#PBS -o /home/plutzer/job_log_files/
+	# #PBS -q old
 
-export ANACONDA_PATH="/act/Anaconda3-2.3.0/bin"
-export PATH=$ANACONDA_PATH:$PATH
+	export ANACONDA_PATH="/act/Anaconda3-2.3.0/bin"
+	export PATH=$ANACONDA_PATH:$PATH
 
-source activate zplab_cluster
-
-python aggregates_crucible.py
+	source activate zplab_cluster
+	cd $PBS_O_WORKDIR
+	python /home/plutzer/code/aggregates_crucible.py
